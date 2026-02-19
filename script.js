c5e3606c6b02a83707e43575c3362b05ef397ccc
3JlouTaTaPuH <0ruslan24rus0@gmail.com>
2026-02-19 15:58:35 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Простая имитация отправки формы
-document.getElementById('contactForm').addEventListener('submit', function(e){
-    e.preventDefault();
-    alert('Спасибо! Ваша заявка отправлена.');
-    this.reset();
-});
